386f209f5435c667766a7ffabf76496ebca05317
SantiagoCor <87431955+SantiagoCor@users.noreply.github.com>
2021-09-04 11:52:56 -0300

    Update PromoAxB.java
    Agregué constructor

--- src/turismoEnLaTierraMedia/PromoAxB.java
+++ src/turismoEnLaTierraMedia/PromoAxB.java
@@ -2,4 +2,9 @@ package turismoEnLaTierraMedia;
 
 public class PromoAxB extends Promocion {
 
+	public PromoAxB(String nombre, Atraccion[] atracciones) {
+		super(nombre, atracciones);
+		// TODO Auto-generated constructor stub
+	}
+
 }
