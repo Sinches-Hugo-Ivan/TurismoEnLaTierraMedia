557bbecaa27a69d3761c3ed221ce0a06ea50ef3d
SantiagoCor <87431955+SantiagoCor@users.noreply.github.com>
2021-09-04 11:51:51 -0300

    Update Producto.java
    Agregué constructor, atributos y metodos

--- src/turismoEnLaTierraMedia/Producto.java
+++ src/turismoEnLaTierraMedia/Producto.java
@@ -1,5 +1,18 @@
 package turismoEnLaTierraMedia;
 
 public abstract class Producto {
+	protected int costoEnMonedas;
+	protected double tiempoEnHoras;
+	private String nombre;
+	private enum tipo { Aventura, Paisaje, Degustación };		//No se usar enums... Hay que agregarlo al constructor
+
+	
+	public Producto(String nombre) {		//AGREGAR ENUM
+		this.nombre = nombre;
+	}
+
+	public abstract boolean consultarDisponibilidad(String user);
+
+	public abstract void comprar(String user);
 
 }
