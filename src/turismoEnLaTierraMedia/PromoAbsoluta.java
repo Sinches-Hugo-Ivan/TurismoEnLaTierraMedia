afa4f832322d69193b57e1484f66bf4730e09b99
SantiagoCor <87431955+SantiagoCor@users.noreply.github.com>
2021-09-04 11:52:24 -0300

    Update PromoAbsoluta.java
    Agregué constructor

--- src/turismoEnLaTierraMedia/PromoAbsoluta.java
+++ src/turismoEnLaTierraMedia/PromoAbsoluta.java
@@ -2,4 +2,9 @@ package turismoEnLaTierraMedia;
 
 public class PromoAbsoluta extends Promocion {
 
+	public PromoAbsoluta(String nombre, Atraccion[] atracciones) {
+		super(nombre, atracciones);
+		// TODO Auto-generated constructor stub
+	}
+
 }
