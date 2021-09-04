3c70318a808467a9deba220e10fe1f7d4d4ec88d
SantiagoCor <87431955+SantiagoCor@users.noreply.github.com>
2021-09-04 11:55:12 -0300

    Update Promocion.java
    Agregué constructor, atributo atraccionesIncluidas y métodos consultarDisponibilidad y comprar

--- src/turismoEnLaTierraMedia/Promocion.java
+++ src/turismoEnLaTierraMedia/Promocion.java
@@ -1,5 +1,20 @@
 package turismoEnLaTierraMedia;
 
-public class Promocion extends Producto {
+public abstract class Promocion extends Producto {
+	
+	protected Atraccion[] atraccionesIncluidas;
 
+	public Promocion(String nombre, Atraccion[] atracciones) {
+		super(nombre);
+		this.atraccionesIncluidas = atracciones;
+	}
+	
+	@Override
+	public boolean consultarDisponibilidad(String user) {
+		return false;
+	}
+
+	@Override
+	public void comprar(String user) {
+	}
 }
