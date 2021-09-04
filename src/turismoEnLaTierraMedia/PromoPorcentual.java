6895e1c8838b1ccb4ad6fbcb2f8b31e4531be48f
SantiagoCor <87431955+SantiagoCor@users.noreply.github.com>
2021-09-04 11:53:25 -0300

    Update PromoPorcentual.java
    Agregué constructor

--- src/turismoEnLaTierraMedia/PromoPorcentual.java
+++ src/turismoEnLaTierraMedia/PromoPorcentual.java
@@ -2,4 +2,9 @@ package turismoEnLaTierraMedia;
 
 public class PromoPorcentual extends Promocion {
 
+	public PromoPorcentual(String nombre, Atraccion[] atracciones) {
+		super(nombre, atracciones);
+		// TODO Auto-generated constructor stub
+	}
+
 }
